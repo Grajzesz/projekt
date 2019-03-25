9b102082e03b357769f9f422cb72b025f57dbfed
Grajzesz <grajzesz@gmail.com>
2019-03-25 15:20:42 +0100

    ok

--- src/js/js.js
+++ src/js/js.js
@@ -1,23 +1,35 @@
+window.addEventListener('load' , function(){
+
+    
+})
+
+
+
+
+
 $(document).ready(function() {
-    $(document).keyup(function(e) {
-        $('#kot').removeClass('minicircle');
-    });
-    var initX = 0;
-    var initY = 0;
-    $(document).keydown(function(e) {
-        if (e.keyCode == 38) {
-            //up
-            $('#kot').addClass('minicircle');
-        } else if (e.keyCode == 37) {
-            //left
-            initX += 10;
-            document.getElementsByTagName('body')[0].style.backgroundPositionX =
-                initX + 'px';
-        } else if (e.keyCode == 39) {
-            //right
-            initX -= 10;
-            document.getElementsByTagName('body')[0].style.backgroundPositionX =
-                initX + 'px';
-        }
-    });
+    // $(document).keyup(function(e) {
+    //     $('#kot').removeClass('minicircle');
+    // });
+    // var initX = 0;
+    // var initY = 0;
+    // $(document).keydown(function(e) {
+    //     if (e.keyCode == 38) {
+    //         //up
+    //         $('#kot').addClass('minicircle');
+    //     } else if (e.keyCode == 37) {
+    //         //left
+    //         initX += 10;
+    //         document.getElementsByTagName('body')[0].style.backgroundPositionX =
+    //             initX + 'px';
+    //     } else if (e.keyCode == 39) {
+    //         //right
+    //         initX -= 10;
+    //         document.getElementsByTagName('body')[0].style.backgroundPositionX =
+    //             initX + 'px';
+    //     }
+    // });
+
+
 });
+
